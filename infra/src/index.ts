c9314e2c059a807ca2f883a91d16b1c2947997f4
Tomy Salminen <tomy.salminen@futurice.com>
2022-03-20 00:01:52 +0200

    Feature/infra (#1)
    * Added infra
    
    * Added prod stack
    
    * Added github actions
    
    * Added environment
    
    * Fix ci
    
    * Fix ci
    
    * Fix CI
    
    * Fix ci
    
    * Fix ci
    
    * Fix ci
    
    * Fix ci
    
    * Update readme
    
    * Update branch trigger
    
    * Update .env.example

---
+++ infra/src/index.ts
@@ -0,0 +1,41 @@
+import { App } from 'aws-cdk-lib';
+import { z } from 'zod';
+import { WappuStack, WappuStage } from './wappu-stack';
+
+const configSchema = z.object({
+  CDK_DEFAULT_REGION: z.string(),
+  CDK_DEFAULT_ACCOUNT: z.string(),
+  PROJECT_NAME: z.string().optional().default('suomensuurinwappu'),
+  // Bundle folder path relative to project root
+  BUNDLE_PATH: z.string().min(1),
+});
+export type IConfig = z.infer<typeof configSchema>;
+const config = configSchema.parse(process.env);
+
+const app = new App();
+
+const devStack = new WappuStack(
+  app,
+  `${config.PROJECT_NAME}-stack-${WappuStage.DEV}`,
+  {
+    config,
+    stage: WappuStage.DEV,
+    env: {
+      region: config.CDK_DEFAULT_REGION,
+      account: config.CDK_DEFAULT_ACCOUNT,
+    },
+  }
+);
+
+const prodStack = new WappuStack(
+  app,
+  `${config.PROJECT_NAME}-stack-${WappuStage.PROD}`,
+  {
+    config,
+    stage: WappuStage.PROD,
+    env: {
+      region: config.CDK_DEFAULT_REGION,
+      account: config.CDK_DEFAULT_ACCOUNT,
+    },
+  }
+);
