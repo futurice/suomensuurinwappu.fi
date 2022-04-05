f9d9ecb575bd00d829ace05942adbac599e9ed49
Noora Punamäki <noora.punamaki@futurice.com>
2022-04-05 12:40:33 +0300

    filter visible focus

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -6,7 +6,7 @@ import { cn } from 'utils';
 export const Filter: FC<FilterProps> = ({ children, checked, ...props }) => (
   <label
     className={cn(
-      'inline-flex cursor-pointer rounded-full border border-cyan-700 py-1 px-3 mr-1 text-sm font-bold',
+      'style-focus mr-1 inline-flex cursor-pointer rounded-full border border-cyan-700 py-1 px-3 text-sm font-bold transition-colors focus-within:ring',
       checked ? 'bg-cyan-700 text-white' : 'bg-white text-cyan-700'
     )}
   >
