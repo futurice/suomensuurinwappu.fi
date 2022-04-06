f3d5649778dd925db82ee9764f025d97b796396d
Noora Punamäki <noora.punamaki@futurice.com>
2022-04-06 14:08:41 +0300

    fix footer missing key

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -18,12 +18,14 @@ export const Footer: VFC = () => {
     <footer className="bg-circles xs:h-20 fixed bottom-0 left-0 right-0 h-16 bg-[center_top] px-4 pt-4 sm:h-24">
       <div className="xs:py-4 m-auto flex h-full max-w-4xl items-stretch gap-4 py-2 sm:gap-8 md:gap-16">
         {adverts.slice(0, 3).map((ad) => (
-          <div className="flex flex-1 items-center justify-center">
+          <div
+            key={ad.content.companyName}
+            className="flex flex-1 items-center justify-center"
+          >
             <img
               className="max-h-full max-w-full object-contain object-center"
               src={`${ad.content.logo.filename}/m/352x0/`}
               alt={ad.content.logo.alt || `${ad.content.companyName} logo`}
-              key={`company-logo-${ad.content.companyName}`}
             />
           </div>
         ))}
