c8b048af100fef3372b8786968bc92f8e9b8c141
Tomy Salminen <tomy.salminen@futurice.com>
2022-04-19 14:25:33 +0300

    More gtm stuff

--- infra/src/wappu-stack.ts
+++ infra/src/wappu-stack.ts
@@ -38,7 +38,12 @@ export class WappuStack extends Stack {
 
     const cspDirectives: string[][] = [
       ['default-src', "'self'", 'https://www.googletagmanager.com'],
-      ['connect-src', "'self'", 'https://gapi.storyblok.com'],
+      [
+        'connect-src',
+        "'self'",
+        'https://gapi.storyblok.com',
+        'https://www.google-analytics.com',
+      ],
       ['font-src', "'self'", 'https:', 'data:'],
       ['style-src', "'self'", 'https:', "'unsafe-inline'"],
       ['upgrade-insecure-requests'],
