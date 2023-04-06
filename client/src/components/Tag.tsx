6e8837d05a26e9b271136cb182fbb4bebb8478a6
Noora Punamäki <16301633+noorapunamaki@users.noreply.github.com>
2023-04-06 13:59:11 +0300

    Feat/bg footer refresh (#31)
    * footer with just futu logo w link on white
    
    * icon illustration bg
    
    * green teemunkierros tag

--- client/src/components/Tag.tsx
+++ client/src/components/Tag.tsx
@@ -1,20 +1,20 @@
 import { FC } from 'react';
 
 export const Tag: FC = ({ children }) => (
-  <div className="text-2xs absolute top-2 -left-[2px] flex h-3.5 items-start font-bold text-cyan-700 md:-left-[3px] md:top-4 md:text-sm">
+  <div className="text-2xs absolute top-2 -left-[2px] flex h-3.5 items-start font-medium tracking-wide text-white md:-left-[3px] md:top-4 md:text-sm">
     <svg
       className="w-[4px] md:w-[6px]"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 4 18"
     >
-      <path className="fill-cyan-700" d="M2 14a2 2 0 1 0 0 4v-4Z" />
+      <path className="fill-green-700" d="M2 14a2 2 0 1 0 0 4v-4Z" />
       <path
-        className="fill-white"
+        className="fill-green-500"
         d="M4 0H2a2 2 0 0 0-2 2v14c0-1.1.9-2 2-2h2V0Z"
       />
     </svg>
-    <div className="flex h-[14px] items-center bg-white pl-1 pr-2 md:h-[21px] md:pl-5">
+    <div className="flex h-[14px] items-center bg-green-500 pl-1 pr-2 md:h-[21px] md:pl-5">
       {children}
     </div>
     <svg
@@ -23,7 +23,7 @@ export const Tag: FC = ({ children }) => (
       viewBox="0 0 4 18"
     >
       <path
-        className="fill-white"
+        className="fill-green-500"
         d="M0 14h3.1c.4 0 .7-.4.5-.7L.3 7.5a1 1 0 0 1 0-1L3.6.7C3.8.4 3.6 0 3 0H0v14Z"
       />
     </svg>
