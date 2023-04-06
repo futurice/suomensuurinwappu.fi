8a27222ddb78d447ee9bd7e9e6353baffe6c0f9c
Noora Punamäki <noora.punamaki@futurice.com>
2023-04-06 16:26:02 +0300

    favourites from context

--- client/src/components/Favourite.tsx
+++ client/src/components/Favourite.tsx
@@ -1,42 +1,41 @@
-import { useGlobalContext } from 'contexts';
+import { useGlobalContext, useFavourite } from 'contexts';
 import { Event } from 'interfaces';
-import { FC, useEffect, useState } from 'react';
+import { FC, useMemo } from 'react';
 
-export const Favourite: FC<{ slug:string, event:Event, size?:string }> = ({ slug, event, size }) => {
-  const [ isFavourite, setIsFavourite ] = useState(false);
-  const [ ariaTranslation, setAriaTranslation ] = useState('');
+export const Favourite: FC<{ slug: string; event: Event; size?: string }> = ({
+  slug,
+  event,
+  size,
+}) => {
+  const { isFavourite, toggleFavourite } = useFavourite(slug);
   const { translation } = useGlobalContext();
 
-  useEffect(() => {
-    const previousFavourites = JSON.parse(localStorage.getItem('WAPPU_FAVOURITES') || '[]');
-    setIsFavourite(previousFavourites.includes(slug));
-  }, [setIsFavourite, slug])
+  const label = useMemo(() => {
+    const str = isFavourite
+      ? translation?.removeFromFavourites
+      : translation?.addToFavourites;
 
-  useEffect(() => {
-    const removeEvent = translation?.removeFromFavourites?.replace('{{EVENT}}', event.title) ?? '';
-    const addEvent = translation?.addToFavourites?.replace('{{EVENT}}', event.title) ?? '';
-    setAriaTranslation(isFavourite ? removeEvent : addEvent);
-  }, [isFavourite, event, translation])
-
-  const addToFavourites = (e: any) => {
-    const previousFavourites = JSON.parse(localStorage.getItem('WAPPU_FAVOURITES') || '[]');
-    let newFavourites = previousFavourites;
-    if (isFavourite) {
-      newFavourites = newFavourites.filter((item: any) => item !== slug);
-    } else {
-      newFavourites = [...previousFavourites, slug];
-    }
-    setIsFavourite(!isFavourite);
-    localStorage.setItem('WAPPU_FAVOURITES', JSON.stringify(newFavourites));
-  }
+    return str?.replace('{{EVENT}}', event.title);
+  }, [event, isFavourite, translation]);
 
   return (
-    <div className="relative h-3.5 z-50 pt-1 pl-2">
-      <button onClick={addToFavourites} className='p-10 absolute right-0 bottom-0' aria-label={ariaTranslation}>
-        <svg fill={isFavourite ? "#6D465B" : "none"} className="stroke-cyan-700 stroke-2 hover:fill-cyan-700 absolute bottom-0 right-0" width={size ?? "20px"} height={size ?? "20px"} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z"/>
+    <div className="relative z-50 h-3.5 pt-1 pl-2">
+      <button
+        onClick={toggleFavourite}
+        className="absolute right-0 bottom-0 p-10"
+        aria-label={label}
+      >
+        <svg
+          fill={isFavourite ? '#6D465B' : 'none'}
+          className="absolute bottom-0 right-0 stroke-cyan-700 stroke-2 hover:fill-cyan-700"
+          width={size ?? '20px'}
+          height={size ?? '20px'}
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z" />
         </svg>
       </button>
     </div>
-  )
+  );
 };
